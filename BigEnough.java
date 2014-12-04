288b0120ec8c877077fc0a4b83e80657e4f2fbd7
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-04 18:37:44 +0000

    initial version

---
+++ BigEnough.java
@@ -0,0 +1,18 @@
+package day6;
+
+public class BigEnough {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		EmployeesArrays ea = new EmployeesArrays ();
+		
+
+		
+		
+	}
+
+}
