9a4c8f4b5f041240dc29c933e672bd99beb83039
BBK-PiJ-2014-86 <andreangeorgiev@yahoo.com>
2014-12-04 18:41:38 +0000

    added Scanner

--- BigEnough.java
+++ BigEnough.java
@@ -1,5 +1,7 @@
 package day6;
 
+import java.util.Scanner;
+
 public class BigEnough {
 
 	/**
@@ -8,9 +10,14 @@ public class BigEnough {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner (System.in);
+		
 		EmployeesArrays ea = new EmployeesArrays ();
 		
-
+		System.out.println("Enter ID and name of employee : ");
+		
+		ea.enterId(sc.nextInt());
+		ea.enterName(sc.nextLine());
 		
 		
 	}
